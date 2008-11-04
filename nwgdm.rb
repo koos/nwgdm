630815cca9c1f5f5374c3cf10f49df560fc6c013
Jan Kus <jan@railslove.com>
2008-11-04 13:46:05 +0100

    fixed sinus sampling code and added autocorrelation code

--- nwgdm.rb
+++ nwgdm.rb
@@ -1,10 +1,24 @@
 class NWGDM
   
+  # //Sinus Abtasten
+  #   sinus = new double[abtastwerte];
+  #   if (sinus == NULL) {
+  #     fprintf(stderr, "new double[abtastwerte] failed.\n");
+  #     return (-1);  
+  #   } 
+  #   double step = (2*PI) / abtastwerte;
+  #   for (int i = 0; i < abtastwerte; i++) {
+  #     sinus[i] = sin(frequenz*i*step);
+  #   }
+  #   plot("sinus", sinus, abtastwerte, 0, (2*PI), "lines", "Sinus", "x", "y");
+  
+  
   def sample_sinus(f_signal, f_sample, duration)
     # f(t) = A*sin(wt+phi) = A*sin(2pi*f*t+phi) = A*sin(2pi*(1/T)*t+phi)
     result = []
-    for i in 0...f_sample*duration do
-        result[i] = Math.sin(f_signal*(2*Math::PI/f_sample*duration)*i)        
+    for i in 0...f_sample*duration do #*duration do
+      result[i] = Math.sin(f_signal*i*(2*Math::PI/f_sample))
+      # result[i] = Math.sin(f_signal*(2*Math::PI/f_sample*duration)*i)        
      end
      result
   end
--- nwgdm_test.rb
+++ nwgdm_test.rb
@@ -6,19 +6,23 @@ require 'array'
 c = NWGDM.new
 
 ################ First lecture: sampling a signal ################
-
+SAMPLES = 500
 # Parameter: Signal Frequence (Hz/s), Sample Frequenze (Hz/s), Duration (e.g. 5 seconds)
 signal_frequence = 1.0
-samples = 44200
+samples = SAMPLES
 duration = 1.0
 
 signal_frequence_1 = 5.0
-samples_1 = 44200
+samples_1 = SAMPLES
 duration_1 = 1.0
 
 signal_frequence_2 = 1.0
-samples_2 = 44200
+samples_2 = SAMPLES
 duration_2 = 5.0
+
+signal_frequence_3 = 2.0
+samples_3 = SAMPLES
+duration_3 = 10.0
 
 puts "Sampling signal Signal frequence: #{signal_frequence}, Samples: #{samples}, Duration: #{duration}..."
 signal = c.sample_sinus(signal_frequence, samples, duration)
@@ -32,6 +36,10 @@ puts "Sampling signal Signal frequence: #{signal_frequence_2}, Samples: #{sample
 signal_2 = c.sample_sinus(signal_frequence_2, samples_2, duration_2)
 puts 'Done'
 
+puts "Sampling signal Signal frequence: #{signal_frequence_3}, Samples: #{samples_3}, Duration: #{duration_3}..."
+signal_3 = c.sample_sinus(signal_frequence_3, samples_3, duration_3)
+puts 'Done'
+# 
 # fh = FileHelper.new('sinus.csv')
 # fh.write_to_file(signal)
 
@@ -40,9 +48,10 @@ puts 'Generate print...'
 sig = Gruff::Line.new
 sig.title = "Signal, #{Time.now}" 
 puts 'Prepraing data...'
-sig.data("Signal, Signal frequence: #{signal_frequence}, Samples: #{samples}, Duration: #{duration}", signal)
-sig.data("Signal, Signal frequence: #{signal_frequence_1}, Samples: #{samples_1}, Duration: #{duration_1}", signal_1)
-sig.data("Signal, Signal frequence: #{signal_frequence_2}, Samples: #{samples_2}, Duration: #{duration_2}", signal_2)
+sig.data("Signal, fs=#{signal_frequence}, samples= #{samples}, T= #{duration}", signal)
+sig.data("Signal, fs=#{signal_frequence_1}, samples=#{samples_1}, T=#{duration_1}", signal_1)
+sig.data("Signal, fs=#{signal_frequence_2}, samples=#{samples_2}, T=#{duration_2}", signal_2)
+sig.data("Signal, fs=#{signal_frequence_3}, samples=#{samples_3}, T=#{duration_3}", signal_3)
 
 puts 'Preparing labels...'
 
@@ -55,21 +64,28 @@ puts 'Preparing labels...'
 sig.labels = {0 => '0', 11000 => '11000', 44000 => '44000'}
 
 puts 'Writing file...'
-sig.write("sinus_comparsion.png")
+sig.write("sinus_sampling.png")
 puts 'Done'
 
 ################ Testing a signal with autocerrelation ################
 
-# puts 'Calculating autocorrelation...'
-# acfs = c.acf(signal, 10000)
-# puts 'Done'
-# 
-# puts 'Printing autocorrelation...'
-# acf = Gruff::Line.new
-# acf.title = "Autokorrelation"
-# acf.data("Autokorrelation", acfs)
-# acf.write('autokorrelation.png')
-# puts 'Done'
+puts 'Calculating autocorrelation...'
+acfs = c.acf(signal, 100)
+acfs_1 = c.acf(signal_1, 100)
+acfs_2 = c.acf(signal_2, 100)
+acfs_3 = c.acf(signal_3, 100)
+puts 'Done'
+
+puts 'Printing autocorrelation...'
+acf = Gruff::Line.new
+acf.title = "Autokorrelation #{Time.now}"
+puts 'Prepraing data...'
+acf.data("Autokorrelation Signal: fs=#{signal_frequence}, samples: #{samples}, T=#{duration}", acfs)
+acf.data("Autokorrelation Signal: fs=#{signal_frequence_1}, samples: #{samples_1}, T=#{duration_1}", acfs_1)
+acf.data("Autokorrelation Signal: fs=#{signal_frequence_2}, samples: #{samples_2}, T=#{duration_2}", acfs_2)
+acf.data("Autokorrelation Signal: fs=#{signal_frequence_3}, samples: #{samples_3}, T=#{duration_3}", acfs_3)
+acf.write('autokorrelation.png')
+puts 'Done'
 
 # fh = FileHelper.new('korrelation.csv')
 # fh.write_to_file(acfs)
